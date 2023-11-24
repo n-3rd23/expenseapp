caa1cff0bacf7c9e2dff8ca465bef292beab3ef4
Alan <62791466+n-3rd23@users.noreply.github.com>
2023-11-24 18:41:47 +0530

    login

--- expenseapp/src/components/shared/Auth/protected-route.tsx
+++ expenseapp/src/components/shared/Auth/protected-route.tsx
@@ -5,8 +5,8 @@ import { Navigate, Outlet } from "react-router-dom";
 export const ProtectedRoute = () => {
   const authenticatedUser = useSelector((state: RootState) => state.user);
   console.log("is authenticated : ", authenticatedUser);
-  if (!authenticatedUser) {
-    return <Navigate to="/login" replace={true} />;
-  }
+  // if (!authenticatedUser) {
+  //   return <Navigate to="/login" replace={true} />;
+  // }
   return <Outlet />;
 };
--- expenseapp/src/redux/store.ts
+++ expenseapp/src/redux/store.ts
@@ -1,11 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "@/redux/user/user.slice";
-import fetcher from "@/lib/utils/fetcher";
-// const persistedState =
-//   JSON.parse(localStorage.getItem("expense_user") as string) ||
-//   (await getUser());
+import { configureStore } from "@reduxjs/toolkit";
+const persistedState =
+  JSON.parse(localStorage.getItem("expense_user") as string) || {};
 
-const persistedState = await getUser();
+// const persistedState = await getUser();
 
 export const store = configureStore({
   reducer: {
@@ -15,14 +13,6 @@ export const store = configureStore({
     user: persistedState,
   },
 });
-
-async function getUser() {
-  const response = await fetcher.get("/auth/user");
-  if (response.status === 200) {
-    return response.data?.data;
-  }
-  return {};
-}
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
